10bfcfb88e3e749eb84dbee37a42171236a3b6d7
Tom Naughton <tomnaughton150@gmail.com>
2025-12-15 20:39:01 +0000

    Basic unit testing

---
+++ ExecAgent.Core.Tests/DockerExecutorTests.cs
@@ -0,0 +1,34 @@
+﻿using ExecAgent.Core.Docker;
+using ExecAgent.Core.Models;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+public class DockerExecutorTests
+{
+    [Fact]
+    public async Task EchoCommand_ReturnsHelloUnitTest()
+    {
+        var executor = new DockerExecutor();
+
+        var request = new ExecRequest
+        {
+            Container = "execagent-test", // Make sure this container exists
+            Command = new[] { "echo", "Hello UnitTest" },
+            Tty = true
+        };
+
+        var session = await executor.CreateSessionAsync(request, CancellationToken.None);
+
+        var outputText = "";
+        await foreach (var output in session.StreamAsync(CancellationToken.None))
+        {
+            outputText += output.Data;
+        }
+
+        var exitCode = await session.GetExitCodeAsync();
+
+        Assert.Contains("Hello UnitTest", outputText);
+        Assert.Equal(0, exitCode);
+    }
+}
---
+++ ExecAgent.Core.Tests/LibraryLogicTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using ExecAgent.Core.Models;
+
+public class LibraryLogicTests
+{
+    [Fact]
+    public void ExecRequest_SetsPropertiesCorrectly()
+    {
+        var request = new ExecRequest
+        {
+            Container = "test-container",  // required
+            Command = new[] { "ls", "-la" }, // required
+            Tty = true
+        };
+
+        Assert.Equal("test-container", request.Container);
+        Assert.Equal(new[] { "ls", "-la" }, request.Command);
+        Assert.True(request.Tty);
+    }
+
+    [Fact]
+    public void ExecRequest_DefaultsAreCorrect()
+    {
+        var request = new ExecRequest
+        {
+            Container = "dummy-container",  // required
+            Command = new string[] { }      // required
+        };
+
+        Assert.Equal("dummy-container", request.Container);
+        Assert.Empty(request.Command);
+        Assert.False(request.Tty);
+    }
+}
